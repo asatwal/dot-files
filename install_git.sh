4f375d704678ae353a7394c43cbddcc60e67a46d
asatwal <asatwal01@googlemail.com>
2016-03-30 15:12:03 +0100

    Create installation file for git utils

---
+++ install_git.sh
@@ -0,0 +1,9 @@
+#!bash
+
+mkdir -p ~/.bash
+
+cd ~/.bash
+git clone git://github.com/jimeh/git-aware-prompt.git
+
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.bash/git-completion.bash
+
